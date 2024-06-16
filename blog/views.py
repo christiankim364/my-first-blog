5e0000845d913ef4bf6eadcb6ec342c4b69fd9cd
Christian <christiankim364@gmail.com>
2024-06-15 22:38:54 -0400

    trying again

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,12 @@ from . import views
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
+def topics(request):
+    return render(request, 'blog/topics.html', {})
+
 def aboutme(request):
     return render(request, 'blog/aboutme.html', {})
 
 def resources(request):
     return render(request, 'blog/resources.html', {})
 
-def topics(request):
-    return render(request, 'blog/topics.html', {})
